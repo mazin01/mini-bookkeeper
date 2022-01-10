2476ff70870fb8898b9a56d84d809f167d471a62
mazin01 <mmmazin01@gmail.com>
2022-01-10 20:29:32 +0200

    added commas and spaces between every three digits

--- script.js
+++ script.js
@@ -61,6 +61,18 @@ function addTransaction(e) {
     };
 }
 
+// adding commas or spaces to group every three digits
+function commafy( num ) {
+    var str = num.toString().split('.');
+    if (str[0].length >= 5) {
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    if (str[1] && str[1].length >= 5) {
+        str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return str.join('.');
+}
+
 // generat random ID
 function generatID() {
     return Math.floor(Math.random() * 100000000)
@@ -79,7 +91,7 @@ function addTransactionDOM(transaction) {
     item.setAttribute("tabindex", "0")
 
     item.innerHTML = `
-    ${transaction.title} <span>${sign}${Math.abs(transaction.amount)}</span> 
+    ${transaction.title} <span>${sign}${commafy(Math.abs(transaction.amount))}</span> 
     <button class="delete-btn" tabindex="0" onClick="removeTransaction(${transaction.id})">x</button>`;
 
     list.appendChild(item);
@@ -95,9 +107,9 @@ function updateValues() {
 
     const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
 
-    balance.innerText = `$${total}`;
-    moneyBlus.innerText = `$${income}`;
-    moneyMinus.innerText = `$${expense}`;
+    balance.innerText = `$${commafy(total)}`;
+    moneyBlus.innerText = `$${commafy(income)}`;
+    moneyMinus.innerText = `$${commafy(expense)}`;
 };
 
 function removeTransaction(id){
